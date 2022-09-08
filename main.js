44ee732fb5d82230647339737a8df81701d4b743
zachary62 <33015448+zachary62@users.noreply.github.com>
2022-09-08 13:03:42 -0400

    2 files modified

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import './style.css';
 
-// Add some content to the HTML
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <h4>Open the DevTools console to see tsshe output</h4>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+// // Add some content to the HTML
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <h4>Open the DevTools console to see the output</h4>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `;
+
+// d3.select('#app').append('hello');
 
 import * as duckdb from '@duckdb/duckdb-wasm';
 import duckdb_wasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url';
@@ -33,46 +35,60 @@ const logger = new duckdb.ConsoleLogger();
 const db = new duckdb.AsyncDuckDB(logger, worker);
 await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
 
-const conn = await db.connect(); // Connect to db
+// Connect to db
+const conn = await db.connect();
 
-let table = 
-// Basic query
-console.log('Basic query');
-let q = await conn.query(`SELECT count(*)::INTEGER as v
-FROM generate_series(0, 100) t(v)`); // Returns v = 101
-console.log('Query result (Arrow Table):', q);
-
-// Copy of query result (JSON instead of Arrow Table)
-console.log(
-  'Query result copy (JSON):',
-  JSON.parse(JSON.stringify(q.toArray()))
+let res = await conn.query(
+  'select * from "https://raw.githubusercontent.com/zachary62/attribution-playbook/master/data/ad_spend.csv"'
 );
-console.log('Query result :', q.toArray());
-console.log('');
-
-// Prepare query
-console.log('Prepared query statement');
-const stmt = await conn.prepare(
-  `SELECT (v + ?) as v FROM generate_series(0, 1000) as t(v);`
-);
-
-// ... and run the query with materialized results
-const res = await stmt.query(234); // Returns 1001 entries ranging from v = 234 to 1,234
 console.log('Statement result (Table):', res);
+
+let jsonTable = JSON.parse(
+  JSON.stringify(
+    res.toArray(),
+    (key, value) => (typeof value === 'bigint' ? value.toString() : value) // return everything else unchanged
+  )
+);
 console.log(
   'Statement result copy (JSON):',
   // Bug fix explained at: https://github.com/GoogleChromeLabs/jsbi/issues/30
-  JSON.parse(
-    JSON.stringify(
-      res.toArray(),
-      (key, value) => (typeof value === 'bigint' ? value.toString() : value) // return everything else unchanged
-    )
-  )
+  jsonTable
 );
 
-// Closing everything
-// await conn.close();
-// await db.terminate();
-// await worker.terminate();
+function getSchema(jsonTable) {
+  return Object.entries(jsonTable).map(([key, _]) => key);
+}
 
-// console.log('Finished!');
+function getValue(jsonTable) {
+  return Object.entries(jsonTable).map(([_, value]) => value);
+}
+
+console.log('Schema:', getSchema(jsonTable));
+
+// add table to the page
+var table = d3
+  .select('#app')
+  .append('table')
+  .attr('class', 'table table-hover');
+var header = table.append('thead').append('tr');
+header
+  .selectAll('th')
+  .data(getSchema(jsonTable[0]))
+  .enter()
+  .append('th')
+  .text(function (d) {
+    return d;
+  });
+var tablebody = table.append('tbody');
+let rows = tablebody.selectAll('tr').data(jsonTable).enter().append('tr');
+
+let cells = rows
+  .selectAll('td')
+  .data(function (d) {
+    return getValue(d);
+  })
+  .enter()
+  .append('td')
+  .text(function (d) {
+    return d;
+  });
